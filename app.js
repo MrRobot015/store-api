afa3447a8b5f23bac1a65ae48f354b5d25c7a9f5
Mujtaba Salah El-Deen <mujtabasalah.dev@gmail.com>
2022-10-28 03:17:10 +0200

    adding log and error handling

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+// *** routes ***
+const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+const { application } = require('express');
+//
 
 const app = express();
 
-app.use((request, response, next) => {
-  response.status(200).json({
-    message: 'Succesfully created',
+//logger
+app.use(morgan('dev'));
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+
+// handle error
+app.use((req, res, next) => {
+  const err = new Error('Route not found');
+  err.status(404);
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    code: error.status || 500,
+    message: error.message,
   });
 });
 
