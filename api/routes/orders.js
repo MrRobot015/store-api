3405d47d2d348cfda3d5904d8aa791a23db9cf2b
Mujtaba Salah El-Deen <mujtabasalah.dev@gmail.com>
2022-10-30 23:47:24 +0200

    Setup orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const { Order } = require('../../models');
 
-router.get('/', (req, res, next) => {
-  res.status(200);
-  res.json({
-    message: `orders/ get`,
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({ include: 'products' });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.post('/', (req, res, next) => {
-  res.status(200);
-  res.json({
-    message: `orders/ post`,
-  });
+router.post('/', async (req, res, next) => {
+  const { order_id, productId, productCount } = req.body;
+  try {
+    const order = await Order.create({ order_id, productId, productCount });
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.get('/:id', (req, res, next) => {
-  res.status(200);
-  res.json({
-    message: `orders/ get ${req.params.id}`,
-  });
+router.get('/:id', async (req, res, next) => {
+  const order_id = req.params.id;
+  try {
+    const order = await Order.findOne({ order_id });
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  res.status(200);
-  res.json({
-    message: `orders/ delete ${req.params.id}`,
-  });
+router.delete('/:id', async (req, res, next) => {
+  const order_id = req.params.id;
+  try {
+    const order = await Order.findOne({ order_id });
+    order.destroy();
+    res.status(200).json({ message: 'order delete successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
